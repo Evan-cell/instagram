b25afc483eed176a70b1045c6c8e83e4accd716d
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-12-08 17:46:55 +0300

    profile

--- kimsta/urls.py
+++ kimsta/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('login/',views.loginPage, name="login"),
     path('',views.registerPage, name="register"),
     path('insta/', views.insta, name='insta'),
+    path('profile/', views.profile, name='profile'),
+    path('search/', views.search_post, name='search.post'), 
+    path('like/', views.like_image, name='like-image'),
+    path('comments/<image_id>', views.comments,name='comments'),
 ]
