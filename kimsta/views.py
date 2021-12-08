f410b6dcf8fe2e733672b8fa397c28891cd796bc
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-12-08 15:04:42 +0300

    registration

--- kimsta/views.py
+++ kimsta/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from email import message
+from django.shortcuts import redirect, render
 from .models import posts #import photos model
 
 def insta(request):
@@ -7,3 +8,16 @@ def insta(request):
     # adding context 
     ctx = {'photo':photo}
     return render(request, 'temps/insta.html', ctx)
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            message.success(request, 'Account was created for ' + user)
+			
+            return redirect('login/')
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)    
