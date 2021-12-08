27f5768e124b07fd79e9a562259e065c1b8e2016
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-12-08 12:38:28 +0300

    models

--- kimsta/models.py
+++ kimsta/models.py
@@ -1,3 +1,12 @@
+import uuid
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class posts(models.Model):
+    # title field
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    caption = models.TextField(max_length=1500, verbose_name='Caption')
+    #image field
+    image = CloudinaryField('image')
+    posted = models.DateTimeField(auto_now_add=True)
