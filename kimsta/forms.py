0fe8790ed8f10aace258930afd169ef854de1410
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-12-08 17:37:05 +0300

    forms

--- kimsta/forms.py
+++ kimsta/forms.py
@@ -1,5 +1,24 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db.models import fields
+from .models import posts,Profile,Comment
+
+class ImageForm(forms.ModelForm):
+    image = forms.ImageField(widget=forms.FileInput(attrs={
+
+        'id': 'imageform', 'class': 'uploadimage'
+
+    }))
+
+    class Meta:
+        model = posts
+        fields = ['image','title','caption']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model=Comment        
+        fields=['comment']   
 
 class CreateUserForm(UserCreationForm):
 	class Meta:
--- kimsta/views.py
+++ kimsta/views.py
@@ -1,6 +1,10 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
+from .models import posts,Likes,Comment,Profile
+from django.contrib.auth.decorators import login_required
+from .forms import ImageForm,CommentForm
+from django.shortcuts import render,redirect, get_object_or_404
 
 from kimsta.forms import CreateUserForm
 from .models import posts #import photos model
@@ -41,4 +45,64 @@ def loginPage(request):
 				messages.info(request, 'Username OR password is incorrect')
 
 		context = {}
-		return render(request, 'accounts/login.html', context)      
+		return render(request, 'accounts/login.html', context)  
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    # get images for the current logged in user
+    image = posts.objects.filter(user_id=current_user.id)
+    # get the profile of the current logged in user
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    form = ImageForm()
+    if request.method == 'POST':
+        form = ImageForm (request.POST , request.FILES)
+        if form.is_valid():
+            form.instance.user = request.user
+            form.save()
+        redirect('profile')
+    return render(request, 'profile.html', {"image": image, "profile": profile , "form": form})
+    
+@login_required(login_url='/accounts/login/')
+def search_post(request):
+    if 'search' in request.GET and request.GET['search']:
+        search_term = request.GET.get('search').lower()
+        images = posts.search_image_name(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'all-photos/search.html', {'found': message, 'images': images})
+    else:
+        message = 'Not found'
+        return render(request, 'all-photos/search.html', {'danger': message})
+    
+def like_image(request):
+    user = request.user
+    if request.method == 'POST':
+        image_id = request.POST.get('image_id')
+        image_pic =posts.objects.get(id=image_id)
+        if user in image_pic.liked.all():
+            image_pic.liked.add(user)
+        else:
+            image_pic.liked.add(user)    
+            
+        like,created =Likes.objects.get_or_create(user=user, image_id=image_id)
+        if not created:
+            if like.value =='Like':
+               like.value = 'Dislike'
+        else:
+               like.value = 'Like'
+
+        like.save()       
+    return redirect('insta')
+
+@login_required
+def comments(request,image_id):
+  form = CommentForm()
+  image = posts.objects.filter(pk = image_id).first()
+  if request.method == 'POST':
+    form = CommentForm(request.POST)
+    if form.is_valid():
+      comment = form.save(commit = False)
+      comment.user = request.user
+      comment.image = image
+      comment.save() 
+  return redirect('insta')                
